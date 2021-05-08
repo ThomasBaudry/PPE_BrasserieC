7c7d7791292066b2bad69ec982dd2a73a5b05ddd
ThomasBaudry <83713003+ThomasBaudry@users.noreply.github.com>
2021-05-08 14:58:33 +0200

    Add files via upload

---
+++ Clemence/Ajouter.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Clemence
+{
+    partial class Ajouter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.aj1_nom = new System.Windows.Forms.Label();
+            this.aj2_nom = new System.Windows.Forms.Label();
+            this.aj3_nom = new System.Windows.Forms.Label();
+            this.aj4_nom = new System.Windows.Forms.Label();
+            this.spe_titre = new System.Windows.Forms.Label();
+            this.aj_nom = new System.Windows.Forms.TextBox();
+            this.aj2_nb = new System.Windows.Forms.TextBox();
+            this.aj3_nb = new System.Windows.Forms.TextBox();
+            this.aj4_nb = new System.Windows.Forms.TextBox();
+            this.aj1_nb = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.aj_spe_nom = new System.Windows.Forms.TextBox();
+            this.aj_spe = new System.Windows.Forms.TextBox();
+            this.spe_nom = new System.Windows.Forms.Label();
+            this.spe_quantite = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.aj5_nom = new System.Windows.Forms.Label();
+            this.aj5_nb = new System.Windows.Forms.TextBox();
+            this.aj_spe_f = new System.Windows.Forms.TextBox();
+            this.spe_fournisseur = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ajouter / Modifier";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom :";
+            // 
+            // aj1_nom
+            // 
+            this.aj1_nom.AutoSize = true;
+            this.aj1_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aj1_nom.Location = new System.Drawing.Point(12, 162);
+            this.aj1_nom.Name = "aj1_nom";
+            this.aj1_nom.Size = new System.Drawing.Size(46, 20);
+            this.aj1_nom.TabIndex = 3;
+            this.aj1_nom.Text = "Eau :";
+            // 
+            // aj2_nom
+            // 
+            this.aj2_nom.AutoSize = true;
+            this.aj2_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aj2_nom.Location = new System.Drawing.Point(208, 162);
+            this.aj2_nom.Name = "aj2_nom";
+            this.aj2_nom.Size = new System.Drawing.Size(77, 20);
+            this.aj2_nom.TabIndex = 4;
+            this.aj2_nom.Text = "Houblon :";
+            // 
+            // aj3_nom
+            // 
+            this.aj3_nom.AutoSize = true;
+            this.aj3_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aj3_nom.Location = new System.Drawing.Point(12, 218);
+            this.aj3_nom.Name = "aj3_nom";
+            this.aj3_nom.Size = new System.Drawing.Size(47, 20);
+            this.aj3_nom.TabIndex = 5;
+            this.aj3_nom.Text = "Malt :";
+            // 
+            // aj4_nom
+            // 
+            this.aj4_nom.AutoSize = true;
+            this.aj4_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aj4_nom.Location = new System.Drawing.Point(208, 218);
+            this.aj4_nom.Name = "aj4_nom";
+            this.aj4_nom.Size = new System.Drawing.Size(65, 20);
+            this.aj4_nom.TabIndex = 6;
+            this.aj4_nom.Text = "Levure :";
+            // 
+            // spe_titre
+            // 
+            this.spe_titre.AutoSize = true;
+            this.spe_titre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spe_titre.Location = new System.Drawing.Point(12, 326);
+            this.spe_titre.Name = "spe_titre";
+            this.spe_titre.Size = new System.Drawing.Size(167, 24);
+            this.spe_titre.TabIndex = 7;
+            this.spe_titre.Text = "Ingrediant special :";
+            this.spe_titre.Click += new System.EventHandler(this.Label8_Click);
+            // 
+            // aj_nom
+            // 
+            this.aj_nom.Location = new System.Drawing.Point(79, 83);
+            this.aj_nom.Name = "aj_nom";
+            this.aj_nom.Size = new System.Drawing.Size(171, 20);
+            this.aj_nom.TabIndex = 8;
+            // 
+            // aj2_nb
+            // 
+            this.aj2_nb.Location = new System.Drawing.Point(291, 164);
+            this.aj2_nb.Name = "aj2_nb";
+            this.aj2_nb.Size = new System.Drawing.Size(100, 20);
+            this.aj2_nb.TabIndex = 9;
+            // 
+            // aj3_nb
+            // 
+            this.aj3_nb.Location = new System.Drawing.Point(96, 220);
+            this.aj3_nb.Name = "aj3_nb";
+            this.aj3_nb.Size = new System.Drawing.Size(100, 20);
+            this.aj3_nb.TabIndex = 10;
+            // 
+            // aj4_nb
+            // 
+            this.aj4_nb.Location = new System.Drawing.Point(291, 220);
+            this.aj4_nb.Name = "aj4_nb";
+            this.aj4_nb.Size = new System.Drawing.Size(100, 20);
+            this.aj4_nb.TabIndex = 11;
+            this.aj4_nb.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // aj1_nb
+            // 
+            this.aj1_nb.Location = new System.Drawing.Point(96, 164);
+            this.aj1_nb.Name = "aj1_nb";
+            this.aj1_nb.Size = new System.Drawing.Size(100, 20);
+            this.aj1_nb.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(309, 80);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(92, 24);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Biere Bio";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // aj_spe_nom
+            // 
+            this.aj_spe_nom.Location = new System.Drawing.Point(79, 368);
+            this.aj_spe_nom.Name = "aj_spe_nom";
+            this.aj_spe_nom.Size = new System.Drawing.Size(100, 20);
+            this.aj_spe_nom.TabIndex = 14;
+            // 
+            // aj_spe
+            // 
+            this.aj_spe.Location = new System.Drawing.Point(291, 368);
+            this.aj_spe.Name = "aj_spe";
+            this.aj_spe.Size = new System.Drawing.Size(100, 20);
+            this.aj_spe.TabIndex = 15;
+            // 
+            // spe_nom
+            // 
+            this.spe_nom.AutoSize = true;
+            this.spe_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spe_nom.Location = new System.Drawing.Point(13, 368);
+            this.spe_nom.Name = "spe_nom";
+            this.spe_nom.Size = new System.Drawing.Size(50, 20);
+            this.spe_nom.TabIndex = 16;
+            this.spe_nom.Text = "Nom :";
+            // 
+            // spe_quantite
+            // 
+            this.spe_quantite.AutoSize = true;
+            this.spe_quantite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spe_quantite.Location = new System.Drawing.Point(207, 366);
+            this.spe_quantite.Name = "spe_quantite";
+            this.spe_quantite.Size = new System.Drawing.Size(78, 20);
+            this.spe_quantite.TabIndex = 17;
+            this.spe_quantite.Text = "Quantité :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(13, 128);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 24);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Quantité :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(154, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 33);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // aj5_nom
+            // 
+            this.aj5_nom.AutoSize = true;
+            this.aj5_nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.aj5_nom.Location = new System.Drawing.Point(12, 283);
+            this.aj5_nom.Name = "aj5_nom";
+            this.aj5_nom.Size = new System.Drawing.Size(156, 20);
+            this.aj5_nom.TabIndex = 20;
+            this.aj5_nom.Text = "*ingrediant special*  :";
+            // 
+            // aj5_nb
+            // 
+            this.aj5_nb.Location = new System.Drawing.Point(220, 283);
+            this.aj5_nb.Name = "aj5_nb";
+            this.aj5_nb.Size = new System.Drawing.Size(100, 20);
+            this.aj5_nb.TabIndex = 21;
+            // 
+            // aj_spe_f
+            // 
+            this.aj_spe_f.Location = new System.Drawing.Point(120, 394);
+            this.aj_spe_f.Name = "aj_spe_f";
+            this.aj_spe_f.Size = new System.Drawing.Size(100, 20);
+            this.aj_spe_f.TabIndex = 22;
+            // 
+            // spe_fournisseur
+            // 
+            this.spe_fournisseur.AutoSize = true;
+            this.spe_fournisseur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spe_fournisseur.Location = new System.Drawing.Point(13, 392);
+            this.spe_fournisseur.Name = "spe_fournisseur";
+            this.spe_fournisseur.Size = new System.Drawing.Size(101, 20);
+            this.spe_fournisseur.TabIndex = 23;
+            this.spe_fournisseur.Text = "Fournisseur :";
+            // 
+            // Ajouter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(431, 474);
+            this.Controls.Add(this.spe_fournisseur);
+            this.Controls.Add(this.aj_spe_f);
+            this.Controls.Add(this.aj5_nb);
+            this.Controls.Add(this.aj5_nom);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.spe_quantite);
+            this.Controls.Add(this.spe_nom);
+            this.Controls.Add(this.aj_spe);
+            this.Controls.Add(this.aj_spe_nom);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.aj1_nb);
+            this.Controls.Add(this.aj4_nb);
+            this.Controls.Add(this.aj3_nb);
+            this.Controls.Add(this.aj2_nb);
+            this.Controls.Add(this.aj_nom);
+            this.Controls.Add(this.spe_titre);
+            this.Controls.Add(this.aj4_nom);
+            this.Controls.Add(this.aj3_nom);
+            this.Controls.Add(this.aj2_nom);
+            this.Controls.Add(this.aj1_nom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Ajouter";
+            this.Text = "Ajouter";
+            this.Load += new System.EventHandler(this.Ajouter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label aj1_nom;
+        private System.Windows.Forms.Label aj2_nom;
+        private System.Windows.Forms.Label aj3_nom;
+        private System.Windows.Forms.Label aj4_nom;
+        private System.Windows.Forms.Label spe_titre;
+        private System.Windows.Forms.TextBox aj_nom;
+        private System.Windows.Forms.TextBox aj2_nb;
+        private System.Windows.Forms.TextBox aj3_nb;
+        private System.Windows.Forms.TextBox aj4_nb;
+        private System.Windows.Forms.TextBox aj1_nb;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox aj_spe_nom;
+        private System.Windows.Forms.TextBox aj_spe;
+        private System.Windows.Forms.Label spe_nom;
+        private System.Windows.Forms.Label spe_quantite;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label aj5_nom;
+        private System.Windows.Forms.TextBox aj5_nb;
+        private System.Windows.Forms.TextBox aj_spe_f;
+        private System.Windows.Forms.Label spe_fournisseur;
+    }
+}
---
+++ Clemence/Ingredient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clemence
+{
+    public class Ingredient
+    {
+        private int idIngredient;
+        private string nomIngredient;
+        private string fournisseur;
+        private string numBio;
+
+        public int IdIngredient { get => idIngredient; set => idIngredient = value; }
+        public string NomIngredient { get => nomIngredient; set => nomIngredient = value; }
+        public string Fournisseur { get => fournisseur; set => fournisseur = value; }
+        public string NumBio { get => numBio; set => numBio = value; }
+
+        public Ingredient(int id, string nom, string fournisseur)
+        {
+            this.idIngredient = id;
+            this.nomIngredient = nom;
+            this.fournisseur = fournisseur;
+            this.numBio = null;
+        }
+        public Ingredient(int id, string nom, string fournisseur, string num)
+        {
+            this.idIngredient = id;
+            this.nomIngredient = nom;
+            this.fournisseur = fournisseur;
+            this.numBio = num;
+        }
+        public Ingredient()
+        {
+
+        }
+        
+    }
+}
